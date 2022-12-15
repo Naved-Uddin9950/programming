cacb122a131b65367f36eaf2454c630d77f9bc19
Naved Uddin <naveduddin83@gmail.com>
2022-12-15 11:20:36 +0530

    Updated BBC News Clone

--- Web Dev/Projects/Major Projects/BBC News Clone/script/index.js
+++ Web Dev/Projects/Major Projects/BBC News Clone/script/index.js
@@ -1,3 +1,12 @@
+// Declarations starts here
+
+var active = [false, false];
+
+
+// Declarations ends here
+
+// Header functions starts here
+
 function hiddenMenu() {
 	document.getElementById("hidden-menu-container").style.display = "flex";
 }
@@ -6,16 +15,14 @@ function closeHiddenMenu() {
 	document.getElementById("hidden-menu-container").style.display = "none";
 }
 
-var active = false;
-
 function moreMenu() {
 
-	if (active === false) {
+	if (active[0] === false) {
 	document.getElementById("grid-container").style.display = "grid";
 	document.getElementById("moreMenuBtn").style.backgroundColor = "white";
 	document.getElementById("moreMenuBtn").style.color = "black";
 	document.getElementById("moreMenuBtn").style.padding = "10px";
-	active = true;		
+	active[0] = true;		
 	}
 
 	else {
@@ -23,6 +30,35 @@ function moreMenu() {
 		document.getElementById("moreMenuBtn").style.backgroundColor = "#B80000";
 		document.getElementById("moreMenuBtn").style.color = "white";
 		document.getElementById("moreMenuBtn").style.padding = "0px 8px";
-		active = false;
+		active[0] = false;
 	}
 }
+
+// Header functions ends here
+// Main functions starts here
+
+function copyLink() {
+	var Url = document.createElement("textarea");
+    Url.innerHTML = window.location.href;
+    Copied = Url.createTextRange();
+    Copied.execCommand("Copy");
+}
+
+function openShare() {
+	if (active[1] === false) {
+		document.getElementById("shareMe").style.display = "block";
+		document.getElementById("pointerGraphic").style.display = "block";
+		active[1] = true;
+	}
+	else {
+		closeShare();
+		active[1] = false;
+	}
+}
+
+function closeShare() {
+	document.getElementById("shareMe").style.display = "none";
+	document.getElementById("pointerGraphic").style.display = "none";
+}
+
+// Main functions ends here
