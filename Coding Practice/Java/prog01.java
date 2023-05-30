5a268e342503f05ba1e202a621eee3754e3c064f
Naved-Uddin9950 <119304160+Naved-Uddin9950@users.noreply.github.com>
2023-05-30 12:39:09 +0000

    added prog01 to java practice

---
+++ Coding Practice/Java/prog01.java
@@ -0,0 +1,8 @@
+public class prog01{
+    public static void main(String[] args){
+        byte age = 18;
+        String name = "Naved";
+        System.out.println("Your name is " + name);
+        System.out.println("Your age is " + age);
+    }
+}
