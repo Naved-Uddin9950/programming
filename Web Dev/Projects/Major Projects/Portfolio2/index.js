50998639957e5a0f9c594184afa7f941b4f26321
Naved-Uddin9950 <119304160+Naved-Uddin9950@users.noreply.github.com>
2023-05-31 14:41:34 +0000

    Fixed mobile navbar

--- Web Dev/Projects/Major Projects/Portfolio2/index.js
+++ Web Dev/Projects/Major Projects/Portfolio2/index.js
@@ -1,3 +1,21 @@
-function menu() {
-    var a = document.getElementsByClassName("")
+// function menu() {
+//     var nav = document.getElementById("navbar");
+    
+//     if (nav.style.display == "none") {
+//         nav.style.display = "flex";
+//     }
+// }
+
+var menu = document.getElementById("menubtn");
+
+menu.onclick = function(){
+    var nav = document.getElementById("navbar");
+
+    if (nav.style.display === "none") {
+        nav.style.display = "flex";
+    }
+
+    else if (nav.style.display === "flex") {
+        nav.style.display = "none";
+    }
 }
