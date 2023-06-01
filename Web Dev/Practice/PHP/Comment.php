dcd6cfdf3575e5b011351e58b57bd3d51e189717
Naved-Uddin9950 <119304160+Naved-Uddin9950@users.noreply.github.com>
2023-06-01 14:40:25 +0000

    Started PHP and practiced comments in it

---
+++ Web Dev/Practice/PHP/Comment.php
@@ -0,0 +1,17 @@
+    <?php
+
+    // Single line comment --- method 1
+    echo "\nHello World! \n";
+
+    #Single line comment --- method 2
+    echo "Hello Universe! \n";
+
+    /*
+    It's
+    multi
+    line
+    comment
+    */
+    echo "HELLO MULTIVERSE! \n";
+
+    ?>
