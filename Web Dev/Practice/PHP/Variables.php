a203c07dc98e48beb6a39c4bdc49cf54b593ef4f
Naved-Uddin9950 <119304160+Naved-Uddin9950@users.noreply.github.com>
2023-06-01 14:52:39 +0000

    Made a php program for accessing variables

---
+++ Web Dev/Practice/PHP/Variables.php
@@ -0,0 +1,17 @@
+<?php
+
+// Declaration of variables
+$name = "Naved Uddin";
+$age = 20;
+$gender = "Male";
+$marks = 76.67;
+$isTrue = true;
+
+// Accessing Variables
+echo "\nName : ".$name;
+echo "\nGender : ".$gender;
+echo "\nAge : ".$age;
+echo "\nMarks : ".$marks;
+echo "\nActive : ".$isTrue;
+
+?>
