00e53b64061b5a996e3115e863ffc036fea270d9
Naved-Uddin9950 <119304160+Naved-Uddin9950@users.noreply.github.com>
2023-06-01 10:57:33 +0000

    change javascript code for mobile menu toggle

--- Web Dev/Projects/Major Projects/Portfolio2/index.js
+++ Web Dev/Projects/Major Projects/Portfolio2/index.js
@@ -1,21 +1,32 @@
-// function menu() {
+// var menu = document.getElementById("menubtn");
+var openMenu = document.getElementById("menubtn");
+var closeMenu = document.getElementById("closebtn");
+var nav = document.getElementById("navbar");
+// var counter = false;
+
+
+// menu.onclick = function() {
 //     var nav = document.getElementById("navbar");
-    
-//     if (nav.style.display == "none") {
+
+//     if(counter === false){
 //         nav.style.display = "flex";
+//         openMenu.style.display = "none";
+//         closeMenu.style.display = "block";
+//         counter = true;
+//         menu = document.getElementById("closebtn");
+//     }
+
+//     else if(counter === true){
+//         nav.style.display = "none";
+//         openMenu.style.display = "block";
+//         closeMenu.style.display = "none";
+//         counter = false;
+//         var menu = document.getElementById("menubtn");
 //     }
 // }
 
-var menu = document.getElementById("menubtn");
 
-menu.onclick = function(){
-    var nav = document.getElementById("navbar");
 
-    if (nav.style.display === "none") {
-        nav.style.display = "flex";
-    }
-
-    else if (nav.style.display === "flex") {
-        nav.style.display = "none";
-    }
+function openMenu(){
+    nav.style.display = "flex";
 }
