e3219dd270f157479f1927458550b6bff5f3e3d9
Naved-Uddin9950 <119304160+Naved-Uddin9950@users.noreply.github.com>
2023-06-16 13:30:18 +0000

    practiced operators in java

---
+++ Coding Practice/Java/operator.java
@@ -0,0 +1,15 @@
+public class operator{
+    public static void main(String[] args){
+        int num1 = 20;
+        int num2 = 20;
+        System.out.println(num1==num2);
+
+        // String name1 = "Naved";
+        // String name2 = "Naved";
+        // System.out.println(name1==name2);
+
+        String name1 = new String("Naved");
+        String name2 = new String("Naved");
+        System.out.println(name1==name2);
+    }
+}
