46f9dbbd053fd5274fa45fb57d6ab0213176639e
Naved Uddin <naveduddin83@gmail.com>
2023-07-07 15:40:07 +0530

    created components without functionality

--- Web Dev/Projects/Major Projects/Portfolio/src/components/Header.jsx
+++ Web Dev/Projects/Major Projects/Portfolio/src/components/Header.jsx
@@ -1,29 +1,15 @@
+import "../styles/header.css";
+
 const Header = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg  bg-primary" data-bs-theme="dark">
+      <nav className="navbar bg-primary navbar-container" data-bs-theme>
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Naved Uddin</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About Me</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Projects</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
+          <a className="navbar-brand">Naved Uddin</a>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-light text-light searchButton fa-solid fa-magnifying-glass" type="submit" id="searchButton"></button>
+          </form>
         </div>
       </nav>
     </>
