0ef257e4798e46192672d39521cd9cc824b7d241
Naved Uddin <naveduddin83@gmail.com>
2023-07-08 10:56:09 +0530

    designed navbar and sidebar

--- Web Dev/Projects/Major Projects/Portfolio/src/components/Header.jsx
+++ Web Dev/Projects/Major Projects/Portfolio/src/components/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
     <>
       <nav className="navbar bg-primary navbar-container" data-bs-theme>
         <div className="container-fluid">
-          <a className="navbar-brand">Naved Uddin</a>
+          <a className="navbar-brand text-shadow">Naved Uddin</a>
           <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="searchField"/>
             <button className="btn btn-outline-light text-light searchButton fa-solid fa-magnifying-glass" type="submit" id="searchButton"></button>
           </form>
         </div>
--- Web Dev/Projects/Major Projects/Portfolio/src/components/SideMenu.jsx
+++ Web Dev/Projects/Major Projects/Portfolio/src/components/SideMenu.jsx
@@ -2,21 +2,19 @@ import "../styles/sideMenu.css";
 
 const sideMenu = () => {
     return (
-        <>
-            <div className="menu-container">
-                <ul>
-                    <div className="bio">
-                        <li id="myPic"><img src="../../public/naved_pic.jpg" alt="Image: Naved Uddin" /></li>
-                        <h4>Naved</h4>
-                        <p>Bio: Aspiring front-end developer.</p>
-                    </div>
-                    <li> About Me</li>
-                    <li>Projects</li>
-                    <li>Contact Me</li>
-                    <li>Hire Me</li>
-                </ul>
-            </div>
-        </>
+        <div className="menu-container">
+            <ul>
+                <div className="bio">
+                    <li id="myPic"><img src="../../public/naved_pic.jpg" alt="Image: Naved Uddin" /></li>
+                    <h4>Naved</h4>
+                    <p>Bio: Aspiring front-end developer.</p>
+                </div>
+                <li> About Me</li>
+                <li>Projects</li>
+                <li>Contact Me</li>
+                <li>Hire Me</li>
+            </ul>
+        </div>
     )
 }
 
