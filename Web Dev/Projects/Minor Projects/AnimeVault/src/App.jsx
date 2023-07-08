9a118e1906f4a365fc02178f8128f39716a0579e
Naved Uddin <naveduddin83@gmail.com>
2023-07-08 11:08:40 +0530

    created Anime Vault

---
+++ Web Dev/Projects/Minor Projects/AnimeVault/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// imported Bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function App() {
+
+  return (
+    <div className="main-container">
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default App
