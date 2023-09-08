76ef45d6dbf0e710930e0576fe525d3e4f6004c0
Naved Uddin <naveduddin83@gmail.com>
2023-09-08 17:49:44 +0530

    created search bar

--- Web Dev/Projects/Major Projects/Tenshura OS/js/index.js
+++ Web Dev/Projects/Major Projects/Tenshura OS/js/index.js
@@ -1,23 +1,40 @@
 // targeted elements and stored them into a variable to access them later
 const apps = document.getElementById("apps-menu-container");
 const appsBtn = document.getElementById("appsBtn");
+const search = document.getElementById("searchBar");
+const searchBtn = document.getElementById("searchBtn");
 
 // stored boolean values in some variables to access and modify them later
 let isMenuOpen = false;
+let isSearchOpen = false;
 
 // Event listeners
-appsBtn.addEventListener("click", openMenu) // opens & closes the Apps Menu/ Start Menu
+appsBtn.addEventListener("click", appsMenu); // opens & closes the Apps Menu/ Start Menu
+searchBtn.addEventListener("click", searchBar); // opens & closes Search Bar at the bottom of the taskbar
 
 // Function to open & close the Apps menu/ Start Menu
-function openMenu() {
+function appsMenu() {
     if (isMenuOpen === false) {
         apps.classList.remove("slide-bottom");
         apps.style = "display: flex";
         apps.classList.add("slide-top");
         isMenuOpen = true
-    }else{
+    } else {
         apps.classList.remove("slide-top");
         apps.classList.add("slide-bottom");
         isMenuOpen = false;
+    }
+}
+
+// Function to open & close the Search Bar
+function searchBar() {
+    if (isSearchOpen === false) {
+        search.style = "display: inline";
+        search.classList.add("fadeInLeft", "animated");
+        isSearchOpen = true;
+    } else {
+        search.classList.remove("fadeInLeft", "animated");
+        search.style = "display: none"
+        isSearchOpen = false;
     }
 }
