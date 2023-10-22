d532784c7f033860e66e4610924eb577816ebd0b
Naved Uddin <naveduddin83@gmail.com>
2023-10-22 09:12:14 +0530

    added firebase to the project

--- Web Dev/Projects/Minor Projects/SimpleCalculator/index.php
+++ Web Dev/Projects/Minor Projects/SimpleCalculator/index.php
@@ -66,6 +66,33 @@
 
 
 
+
+    <!-- Firebase Scripts -->
+
+    <script type="module">
+        // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+            apiKey: "AIzaSyB3b7jHtIhifMGBZAZWOR9NUmEE9v2rzyw",
+            authDomain: "karlo-calc.firebaseapp.com",
+            projectId: "karlo-calc",
+            storageBucket: "karlo-calc.appspot.com",
+            messagingSenderId: "393769000604",
+            appId: "1:393769000604:web:daacce11a5dcfb87205759",
+            measurementId: "G-GRNSE4RX4G"
+        };
+
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+    </script>
+
 </body>
 
 </html>
