c967c775270f13e84001ce74b5240c1c658af64b
Naved Uddin <naveduddin83@gmail.com>
2024-03-17 13:02:58 +0530

    Day 17 of Java + DSA

---
+++ Coding Practice/Java/Core_Java/Day18/CumulativeLength.java
@@ -0,0 +1,22 @@
+// Homework problem 01 :
+// Take an array of Strings input from the user & find the cumulative (combined) length of all those strings.
+
+package Core_Java.Day18;
+import java.util.Scanner;
+
+public class CumulativeLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+        sc.nextLine();
+        String[] str = new String[size];
+        int len = 0;
+        for(int i=0; i<size; i++) {
+            System.out.printf("Enter value of string of index %d : ", i);
+            str[i] = sc.nextLine();
+            len += str[i].length();
+        }
+        System.out.printf("Cumulative length of all the strings in array is : %d", len);
+    }
+}
---
+++ Coding Practice/Java/Core_Java/Day18/Email.java
@@ -0,0 +1,26 @@
+// Homework problem 03 :
+// Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.
+// Example :
+//          email = “apnaCollegeJava@gmail.com” ; username = “apnaCollegeJava”
+//          email = “helloWorld123@gmail.com”; username = “helloWorld123”
+
+package Core_Java.Day18;
+import java.util.Scanner;
+
+public class Email {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your email : ");
+        String email = sc.next();
+        String user = "";
+        for(int i=0; i<email.length(); i++) {
+            if(email.charAt(i) == '@') {
+                break;
+            } else {
+                user += email.charAt(i);
+            }
+        }
+        System.out.printf("Email : %s \n", email);
+        System.out.printf("Username : %s", user);
+    }
+}
